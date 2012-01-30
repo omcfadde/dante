59a2b38dc90d1f3e81133b6f5c5c11d8612f9114
Oliver McFadden <omcfadde@gmail.com>
2012-01-30 13:36:41 +0200

    x64: game/gamesys/Event.cpp: D_EVENT_STRING
    Signed-off-by: Oliver McFadden <omcfadde@gmail.com>

--- neo/d3xp/gamesys/Event.cpp
+++ neo/d3xp/gamesys/Event.cpp
@@ -813,6 +813,7 @@ void idEvent::Save(idSaveGame *savefile)
 	byte *dataPtr;
 	bool validTrace;
 	const char	*format;
+	idStr s;
 
 	savefile->WriteInt(EventQueue.Num());
 
@@ -843,6 +844,12 @@ void idEvent::Save(idSaveGame *savefile)
 				case D_EVENT_VECTOR :
 					savefile->WriteVec3(*reinterpret_cast<idVec3 *>(dataPtr));
 					size += sizeof(idVec3);
+					break;
+				case D_EVENT_STRING :
+					s.Clear();
+					s.Append(reinterpret_cast<char *>(dataPtr));
+					savefile->WriteString(s);
+					size += MAX_STRING_LEN;
 					break;
 				case D_EVENT_TRACE :
 					validTrace = *reinterpret_cast<bool *>(dataPtr);
@@ -904,6 +911,7 @@ void idEvent::Restore(idRestoreGame *savefile)
 	byte *dataPtr;
 	idEvent	*event;
 	const char	*format;
+	idStr s;
 
 	savefile->ReadInt(num);
 
@@ -965,6 +973,11 @@ void idEvent::Restore(idRestoreGame *savefile)
 					case D_EVENT_VECTOR :
 						savefile->ReadVec3(*reinterpret_cast<idVec3 *>(dataPtr));
 						size += sizeof(idVec3);
+						break;
+					case D_EVENT_STRING :
+						savefile->ReadString(s);
+						idStr::Copynz(reinterpret_cast<char *>(dataPtr), s, MAX_STRING_LEN);
+						size += MAX_STRING_LEN;
 						break;
 					case D_EVENT_TRACE :
 						savefile->ReadBool(*reinterpret_cast<bool *>(dataPtr));
