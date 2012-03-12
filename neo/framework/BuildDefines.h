de9b1eb4e67fa10b8aafd05f33b8508690222987
Oliver McFadden <omcfadde@gmail.com>
2012-03-12 16:40:35 +0200

    SConstruct: ID_ENFORCE_KEY=0
    Signed-off-by: Oliver McFadden <omcfadde@gmail.com>

--- neo/framework/BuildDefines.h
+++ neo/framework/BuildDefines.h
@@ -110,11 +110,7 @@ If you have questions concerning this license or the applicable additional terms
 #endif
 
 #ifndef ID_ENFORCE_KEY
-#	if !defined( ID_DEDICATED ) && !defined( ID_DEMO_BUILD )
-#		define ID_ENFORCE_KEY 1
-#	else
-#		define ID_ENFORCE_KEY 0
-#	endif
+#	define ID_ENFORCE_KEY 0
 #endif
 
 #ifndef ID_OPENAL
