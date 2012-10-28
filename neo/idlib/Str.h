e6ffce864b3392e08403344fd4755e53c85679e1
Andreas Bergmeier <andreas.bergmeier@gmx.net>
2012-10-28 11:03:26 +0100

    Undef stricmp and strnicmp before redefining to avoid warning messages.

--- neo/idlib/Str.h
+++ neo/idlib/Str.h
@@ -61,9 +61,15 @@ If you have questions concerning this license or the applicable additional terms
 #endif
 #define StrCmpNI		use_idStr_Icmpn
 
+#ifdef stricmp
+#undef stricmp
+#endif
 #define stricmp			idStr::Icmp		// use_idStr_Icmp
 #define _stricmp		use_idStr_Icmp
 #define strcasecmp		use_idStr_Icmp
+#ifdef strnicmp
+#undef strnicmp
+#endif
 #define strnicmp		use_idStr_Icmpn
 #define _strnicmp		use_idStr_Icmpn
 #define _memicmp		use_idStr_Icmpn
